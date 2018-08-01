d7a46e71f9d6c4fc6a27520dc625c7a8c2da7841
vijaypraju04 <vijaypraju04@gmail.com>
2018-07-31 22:59:30 -0700

    rendering header in app component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Header from './Components/Header'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to Vijays World</h1>
-        </header>
+        <Header />
       </div>
     );
   }
