1a4fa5eb63fbedc84d41c74bf3dc9bf9f123d49b
vijaypraju04 <vijaypraju04@gmail.com>
2018-08-07 16:35:13 -0700

    header fix

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,7 +1,14 @@
 import React from 'react'
 
 const Header = () => {
-  <Header>Testing</Header>
+  <Header>
+  <div>
+    Header
+  </div>
+  <li>
+    Vijays Website
+  </li>
+  </Header>
 }
 
 export default Header;
