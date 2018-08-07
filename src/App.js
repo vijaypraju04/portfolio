41ad1c6fb14fdf5b4b79cda106433c00b93fe8e0
vijaypraju04 <vijaypraju04@gmail.com>
2018-08-07 16:32:24 -0700

    imported landing

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Header from './Components/Header'
+import Landing from './Components/Landing'
 
 class App extends Component {
   render() {
