9d7de9d0ab26db9362254532fb423580657a7dac
vijaypraju04 <vijaypraju04@gmail.com>
2018-08-07 16:33:43 -0700

    fixed app text

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
       <div className="App">
         <Header />
         <Landing />
-        <p>Hello World</p>
+        <p>Vijays Portfolio</p>
       </div>
     );
   }
