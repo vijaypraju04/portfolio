59215d0e40cba8bc981809f225ab37cc04f2dc55
vijaypraju04 <vijaypraju04@gmail.com>
2018-08-14 21:50:33 -0700

    removed skills import

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './Components/Header'
 import Landing from './Components/Landing'
-import Skills from './Components/Skills'
 
 class App extends Component {
   render() {
