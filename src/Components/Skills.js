880b42e12cc4ed4826efe0a04c961b50a0801890
vijaypraju04 <vijaypraju04@gmail.com>
2018-08-28 22:17:32 -0700

    frameworks added

--- src/Components/Skills.js
+++ src/Components/Skills.js
@@ -4,10 +4,18 @@ const Skills = () => {
   return(
     <div>
     <h3>Languages</h3>
-    <ul>
-    <li>Javascript</li>
-    <li>Ruby</li>
-    </ul>
+      <ul>
+        <li>Javascript</li>
+        <li>Ruby</li>
+      </ul>
+
+    <h3>Frameworks</h3>
+      <ul>
+        <li>React</li>
+        <li>Redux</li>
+        <li>Rails</li>
+        <li>Express</li>
+      </ul>
     </div>
   )
 }
