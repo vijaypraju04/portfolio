8c92929cbaf96db8a16047a6927c170cd42912b3
vijaypraju04 <vijaypraju04@gmail.com>
2018-08-28 22:20:10 -0700

    database experience added

--- src/Components/Skills.js
+++ src/Components/Skills.js
@@ -3,19 +3,26 @@ import React from 'react'
 const Skills = () => {
   return(
     <div>
-    <h3>Languages</h3>
-      <ul>
-        <li>Javascript</li>
-        <li>Ruby</li>
-      </ul>
+      <h3>Languages</h3>
+        <ul>
+          <li>Javascript</li>
+          <li>Ruby</li>
+        </ul>
 
-    <h3>Frameworks</h3>
-      <ul>
-        <li>React</li>
-        <li>Redux</li>
-        <li>Rails</li>
-        <li>Express</li>
-      </ul>
+      <h3>Frameworks</h3>
+        <ul>
+          <li>React</li>
+          <li>Redux</li>
+          <li>Rails</li>
+          <li>Express</li>
+        </ul>
+
+      <h3>Databases</h3>
+        <ul>
+          <li>MySQL<li>
+          <li>MongoDB<li>
+          <li>Postgres</li>
+        </ul>
     </div>
   )
 }
