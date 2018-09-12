8ba344811797dcc0d4ff35ee6291dbd07899e32d
vijaypraju04 <vijaypraju04@gmail.com>
2018-09-11 23:02:59 -0700

    render project function

--- src/Components/ProjectList.js
+++ src/Components/ProjectList.js
@@ -2,11 +2,16 @@ import React from 'react'
 import ProjectItem from './ProjectItem'
 
 const ProjectList = () => {
+
+
+  const renderProject = () => {
+  }
+
   return (
     <div>
       <h1> Projects </h1>
         <ul>
-          <li>feedBack</li>
+          <li onClick={() => {this.renderProject(feedBack)}}>feedBack</li>
           <li>ActiVision</li>
           <li>Network Analyzer</li>
         </ul>
