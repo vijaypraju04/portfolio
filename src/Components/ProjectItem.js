2f37632f262b4779d77dfca8527f07eb6c67c749
vijaypraju04 <vijaypraju04@gmail.com>
2018-09-11 20:15:22 -0700

    project detail skeleton created

--- src/Components/ProjectItem.js
+++ src/Components/ProjectItem.js
@@ -1,5 +1,11 @@
 import React from 'react'
 
 const ProjectItem = () => {
-  
+  return (
+    <div>
+      <p>Link to Github/Project</p>
+      <p>Image of Project Here</p>
+      <p>Description</p>
+    </div>
+  )
 }
